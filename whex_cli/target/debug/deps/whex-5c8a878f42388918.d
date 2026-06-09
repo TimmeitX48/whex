5bc17c68824dc4833c80597d102e2256f9969364
TimmeitX48 <timmejt646@gmail.com>
2026-06-09 14:24:03 +0300

    Delete whex_cli directory

--- whex_cli/target/debug/deps/whex-5c8a878f42388918.d
+++
@@ -1,5 +0,0 @@
-/home/timmeitx48/Desktop/hex viewer/whex_cli/target/debug/deps/whex-5c8a878f42388918.d: src/main.rs
-
-/home/timmeitx48/Desktop/hex viewer/whex_cli/target/debug/deps/libwhex-5c8a878f42388918.rmeta: src/main.rs
-
-src/main.rs:
